3382930018f73bef74a693bd16ba8b29bc486cc0
aueaungoor <auesugoor@gmail.com>
2025-04-21 17:24:05 +0700

    cross

--- src/main/java/work1/demo/MyConfiguration.java
+++ src/main/java/work1/demo/MyConfiguration.java
@@ -12,6 +12,8 @@ public class MyConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+
+                
                 .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
