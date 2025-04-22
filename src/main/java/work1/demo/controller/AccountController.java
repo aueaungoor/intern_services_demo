1ceb4a3b12e963c2257ffb940ec63244f004fad9
aueaungoor <auesugoor@gmail.com>
2025-04-22 10:45:43 +0700

    test

--- src/main/java/work1/demo/controller/AccountController.java
+++ src/main/java/work1/demo/controller/AccountController.java
@@ -341,7 +341,6 @@ public boolean checkLogin(@RequestBody Map<String, String> loginData) {
 
  @PostMapping("/search")
  public ResponseEntity <Map<String , Object>> search(@RequestBody Account key)
- 
  {
    try{
     List<Account> result = accountService.search(key);
